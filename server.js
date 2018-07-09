2871d2a759ba656d4643cc18883e2261c4e85884
Muhammad Syarif <syaripinn@gmail.com>
2018-07-09 22:19:27 +0700

    compacting the code

--- server.js
+++ server.js
@@ -9,12 +9,10 @@ function secondsToHms(d){
     d = Number(d);
     var h = Math.floor(d/3600);
     var m = Math.floor(d % 3600 / 60);
-    var s = Math.floor(d % 3600 / 60);
 
     var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-    return hDisplay + mDisplay + sDisplay; 
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
+    return hDisplay + mDisplay;
 }
 
 // make a simple response
@@ -22,22 +20,13 @@ client.on("message", (message) => {
     if (message.content.startsWith("!luigi")) {
       message.channel.send("Number One!");
     }
-  });
-
-client.login(config.token);
-
-console.log("I'm Ready!"); //startup message
-console.log(Date());
-
-//make salmon run schedule response
-client.on("message", (message) => {
     if (message.content.startsWith("!salmon")) {
 
         request({url: url, json: true},
             function (error, response, body) {
             if (!error && response.statusCode === 200) {
             
-                console.log('successfully get json from splatoon2.ink') // Print the json response
+                console.log('successfully get json from splatoon2.ink'); // Print the json response
 
                 //set the variables to get
                 var currentDate = new Date();                                               //get today time
@@ -47,7 +36,7 @@ client.on("message", (message) => {
                 var end0 = new Date(end0Epoch);
                 var diffStart = start0.getTime() / 1000 - currentDate.getTime() / 1000;     //get time remaining until start of next salmon
                 var diffEnd = end0.getTime() / 1000 - currentDate.getTime() / 1000;         //get time remaining until end of current salmon
-                var mapsName = body.details[0].stage.name                                   //get current map name
+                var mapsName = body.details[0].stage.name;                                   //get current map name
                 var currentWeapon1 = body.details[0].weapons[0].weapon.name;                //get current weapon names
                 var currentWeapon2 = body.details[0].weapons[1].weapon.name;
                 var currentWeapon3 = body.details[0].weapons[2].weapon.name;
@@ -75,7 +64,7 @@ client.on("message", (message) => {
                     nextSchedule = "Ends in " + secondsToHms(diffEnd);
                 } else {
                     nextSchedule = "Starts in " + secondsToHms(diffStart);
-                };
+                }
                 console.log("nextSchedule " + nextSchedule); //print result in console
 
                 message.channel.send({embed: {
@@ -104,6 +93,87 @@ client.on("message", (message) => {
         });
     }
   });
+
+client.login(config.token);
+
+console.log("I'm Ready!"); //startup message
+console.log(Date());
+
+//make salmon run schedule response
+/*client.on("message", (message) => {
+    if (message.content.startsWith("!salmon")) {
+
+        request({url: url, json: true},
+            function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+            
+                console.log('successfully get json from splatoon2.ink'); // Print the json response
+
+                //set the variables to get
+                var currentDate = new Date();                                               //get today time
+                var start0Epoch = body.schedules[0].start_time * 1000;                      //get salmon schedule in Epoch and then convert Epoch time to JS time
+                var end0Epoch = body.schedules[0].end_time * 1000;
+                var start0 = new Date(start0Epoch);                                         //convert JS time to zulu time
+                var end0 = new Date(end0Epoch);
+                var diffStart = start0.getTime() / 1000 - currentDate.getTime() / 1000;     //get time remaining until start of next salmon
+                var diffEnd = end0.getTime() / 1000 - currentDate.getTime() / 1000;         //get time remaining until end of current salmon
+                var mapsName = body.details[0].stage.name;                                   //get current map name
+                var currentWeapon1 = body.details[0].weapons[0].weapon.name;                //get current weapon names
+                var currentWeapon2 = body.details[0].weapons[1].weapon.name;
+                var currentWeapon3 = body.details[0].weapons[2].weapon.name;
+                var currentWeapon4 = body.details[0].weapons[3].weapon.name;
+                //var weaponArray = [currentWeapon1, currentWeapon2, currentWeapon3, currentWeapon4];                   //Unused
+                var weaponString = currentWeapon1 + ", " + currentWeapon2 + ", " + currentWeapon3 + ", " + currentWeapon4;  //concatenate all weapon into one string
+
+                //debug making sure the variables are correct
+                console.log(start0); 
+                console.log(end0);
+                console.log(currentDate);
+                console.log(diffStart);
+                console.log(diffEnd);
+                console.log(weaponString);
+
+                //convert to date
+                var d1 = new Date( new Date(start0Epoch).toUTCString() ).toLocaleDateString(); //Convert JS-Epoch to local date
+                var t1 = new Date( new Date(start0Epoch).toUTCString() ).toLocaleTimeString(); //Convert JS-Epoch to local time
+                var d2 = new Date( new Date(end0Epoch).toUTCString() ).toLocaleDateString();
+                var t2 = new Date( new Date(end0Epoch).toUTCString() ).toLocaleTimeString();
+
+                var nextSchedule = "";                                                          //Set nextSchedule variable
+
+                if(diffStart < 0){
+                    nextSchedule = "Ends in " + secondsToHms(diffEnd);
+                } else {
+                    nextSchedule = "Starts in " + secondsToHms(diffStart);
+                }
+                console.log("nextSchedule " + nextSchedule); //print result in console
+
+                message.channel.send({embed: {
+                    color: 3447003,
+                    title: "Salmon run schedule",
+                    description: nextSchedule,
+                    fields: [
+                        {
+                        name: "Current map",
+                        value: mapsName
+                        },
+                        {
+                        name: "Weapon",
+                        value: weaponString
+                        },
+                        {
+                        name: "About this bot",
+                        value: "[A Salmon run Schedule bot](https://github.com/swawif/salmonrun-discordbot)"
+                        },
+                    ],
+                  }
+
+                });
+            }
+        
+        });
+    }
+  });*/
 
 /* HALL OF SHAME - my personal experimentation code block - DON'T JUDGE ME ;-;
 
