313a1f26e3136a97c743bf419cbb8317509f19cd
Muhammad Syarif <syaripinn@gmail.com>
2018-07-12 00:12:02 +0700

    final version of the module responsible to prepare the responses

--- app/prepareResponse.js
+++ app/prepareResponse.js
@@ -1,40 +1,122 @@
 /*jshint esversion: 6 */
+//this is the script responsible for creating the responses
 const coopSchedule = require('../coop.json');
 
-exports.scheduleResponse = function prepareResponse(num) {
-    var currentDate = new Date();                                                   //get today time
-    var start0Epoch = coopSchedule.schedules[num].start_time * 1000;                        //get salmon schedule in Epoch and then convert Epoch time to JS time
+function secondsToHms(d){
+    d = Number(d);
+    var h = Math.floor(d/3600);
+    var m = Math.floor(d % 3600 / 60);
+
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
+    return hDisplay + mDisplay;
+}
+
+exports.timeUntilSalmon = function timeUntil(num){
+
+    var currentDate = new Date();
+    var start0Epoch = coopSchedule.schedules[num].start_time * 1000;
     var end0Epoch = coopSchedule.schedules[num].end_time * 1000;
-    var start0 = new Date(start0Epoch);                                             //convert JS time to zulu (Z) time
+    var start0 = new Date(start0Epoch);
     var end0 = new Date(end0Epoch);
-    var diffStart = start0.getTime() / 1000 - currentDate.getTime() / 1000;         //get time remaining until start of next salmon
-    var diffEnd = end0.getTime() / 1000 - currentDate.getTime() / 1000;             //get time remaining until end of current salmon
-    var mapsName = coopSchedule.details[num].stage.name;                                    //get current map name
-    var currentWeapon1 = coopSchedule.details[num].weapons[0].weapon.name;                  //get current weapon names
-    var currentWeapon2 = coopSchedule.details[num].weapons[1].weapon.name;
-    var currentWeapon3 = coopSchedule.details[num].weapons[2].weapon.name;
-    var currentWeapon4 = coopSchedule.details[num].weapons[3].weapon.name;
+    var timeUntil = "";
 
-    var weaponString = currentWeapon1 + ", " + currentWeapon2 + ", " + currentWeapon3 + ", " + currentWeapon4;  //concatenate all weapon into one gigantic string
-    var imageLink = '';
 
-    switch(mapsName){
+
+    var diffStart = start0.getTime() / 1000 - currentDate.getTime() / 1000;
+    var diffEnd = end0.getTime() / 1000 - currentDate.getTime() / 1000;
+
+    var d1 = new Date( new Date(start0Epoch).toUTCString() ).toLocaleDateString();  //Convert JS-Epoch to local date
+    var t1 = new Date( new Date(start0Epoch).toUTCString() ).toLocaleTimeString();  //Convert JS-Epoch to local time
+    var d2 = new Date( new Date(end0Epoch).toUTCString() ).toLocaleDateString();
+    var t2 = new Date( new Date(end0Epoch).toUTCString() ).toLocaleTimeString();
+
+    if(diffStart < 0){                                                              //Adjust timeUntil according to current salmon run time (Available or not)
+        timeUntil = "Ends in " + secondsToHms(diffEnd);                                     
+    } else {
+        timeUntil = "Starts in " + secondsToHms(diffStart);
+    }
+
+    console.log(currentDate, start0, start0Epoch, end0, end0Epoch, timeUntil);
+
+    return timeUntil;
+
+};
+
+//console.log("TIMEUNTIL : " +timeUntil(2));
+
+exports.nextSchedules = function nextSalmon(num){
+
+    var currentDate = new Date();
+    var start0Epoch = coopSchedule.schedules[num].start_time * 1000;
+    var end0Epoch = coopSchedule.schedules[num].end_time * 1000;
+    var start0 = new Date(start0Epoch);
+    var nextSchedule = "";
+
+    var diffStart = start0.getTime() / 1000 - currentDate.getTime() / 1000;
+
+    var d1 = new Date( new Date(start0Epoch).toUTCString() ).toLocaleDateString();  //Convert JS-Epoch to local date
+    var t1 = new Date( new Date(start0Epoch).toUTCString() ).toLocaleTimeString();  //Convert JS-Epoch to local time
+    var d2 = new Date( new Date(end0Epoch).toUTCString() ).toLocaleDateString();
+    var t2 = new Date( new Date(end0Epoch).toUTCString() ).toLocaleTimeString();
+
+    if(diffStart < 0){                                                              //Adjust timeUntil according to current salmon run time (Available or not)
+        nextSchedule = "Ends at " + d2 + ", " + t2;                                        
+    } else {
+        nextSchedule = "Starts at " + d1 + ", " + t1;
+    }
+
+    console.log(nextSchedule);
+
+    return nextSchedule;
+
+};
+
+exports.mapName = function mapNames(a) {
+    var currentMap = "";
+    if(a>1){
+        console.log("Error! - No map has been assigned for this schedule yet");
+        currentMap = "No map has been assigned for this schedule yet";
+    }
+    currentMap = coopSchedule.details[0].stage.name;
+
+    console.log(currentMap);
+
+    return currentMap;
+};
+
+exports.weaponName = function weapons(a){
+    var currentWeapon1 = coopSchedule.details[0].weapons[0].weapon.name;                //get current weapon names
+    var currentWeapon2 = coopSchedule.details[0].weapons[1].weapon.name;
+    var currentWeapon3 = coopSchedule.details[0].weapons[2].weapon.name;
+    var currentWeapon4 = coopSchedule.details[0].weapons[3].weapon.name;
+
+    var weaponString = currentWeapon1 + ", " + currentWeapon2 + ", " + currentWeapon3 + ", " + currentWeapon4;
+    console.log(weaponString);
+    return weaponString;
+};
+
+exports.imageLink = function image(a){
+    var currentMap = coopSchedule.details[0].stage.name;
+    var mapImage = "";
+    switch(currentMap){
         default:
-            imageLink = 'https://cdn.wikimg.net/en/splatoonwiki/images/5/5f/Salmon_Run_logo.png';
+            mapImage = 'https://cdn.wikimg.net/en/splatoonwiki/images/5/5f/Salmon_Run_logo.png';
             break;
         case "Salmonid Smokeyard" : 
-            imageLink = 'https://cdn.wikimg.net/en/splatoonwiki/images/c/c7/S2_Stage_Salmonid_Smokeyard.png';
+            mapImage = 'https://cdn.wikimg.net/en/splatoonwiki/images/c/c7/S2_Stage_Salmonid_Smokeyard.png';
             break;
         case "Spawning Grounds" : 
-            imageLink = 'https://cdn.wikimg.net/en/splatoonwiki/images/2/29/S2_Stage_Spawning_Grounds.png';
+            mapImage = 'https://cdn.wikimg.net/en/splatoonwiki/images/2/29/S2_Stage_Spawning_Grounds.png';
             break;
         case "Marooner's Bay" : 
-            imageLink = 'https://cdn.wikimg.net/en/splatoonwiki/images/6/6c/S2_Stage_Marooner%27s_Bay.png';
+            mapImage = 'https://cdn.wikimg.net/en/splatoonwiki/images/6/6c/S2_Stage_Marooner%27s_Bay.png';
             break;
         case "Lost Outpost" : 
-            imageLink = 'https://cdn.wikimg.net/en/splatoonwiki/images/6/68/S2_Stage_Lost_Outpost.png';
+            mapImage = 'https://cdn.wikimg.net/en/splatoonwiki/images/6/68/S2_Stage_Lost_Outpost.png';
             break;
 
     }
-
+    console.log(mapImage);
+    return mapImage;
 };
