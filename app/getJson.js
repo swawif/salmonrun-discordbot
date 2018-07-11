475d28bef404a96774422eb218cb8bd015071154
syarif <syaripinn@gmail.com>
2018-07-11 22:35:00 +0700

    phase one modularization. getJson is now its own module

---
+++ app/getJson.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const fs = require('fs');
+const coopUrl = "https://splatoon2.ink/data/coop-schedules.json";
+
+exports.getCoopSchedule = function getCoopJson(){
+    fs.stat('coop.json', (err) => {
+      if (err){
+        console.log("No JSON Found!");
+      } else {console.log("coop.json found! updating...")};
+    });
+    request({url: coopUrl, json: true},
+      function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        console.log("successfully retrieve coop-schedules.json from splatoon2.ink");
+        console.log(body);
+        var coopJson = body;
+        fs.writeFile('coop.json', JSON.stringify(coopJson), (err) => {
+          if (err) throw err;
+          console.log('coop.json has been updated at ' + Date());
+        });
+      }
+    });
+  };
